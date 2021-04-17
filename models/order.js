843349fd37134770cbce10f4d5d8ee46625a213f
Tejas Guduru <tejas.guduru@outlook.com>
2021-04-17 20:08:32 +0530

    Added order route

--- models/order.js
+++ models/order.js
@@ -17,6 +17,11 @@ const OrderSchema = mongoose.Schema({
     transaction_id: {},
     amount: { type: Number },
     address: String,
+    status: {
+        type: String,
+        default: "Recieved",
+        enum:["Cancelled","Delivered","Shipped","Processing","Recieved"]
+    },
     updated: Date,
     user: {
         type: ObjectId,
